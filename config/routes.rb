acc4ab185969994fcd63c0aaff4f53e350fa6fdc
TeppeiYamashita <143589587+51492@users.noreply.github.com>
2023-09-10 11:19:19 +0000

    [update]app1-7

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   # HTTPメソッド 'URL' => 'コントローラ#アクション'
+  
+  get "/top" => "homes#top"
+  
   get 'lists/new' #get:データを取得するためのHTTPメソッド
     post "lists" => "lists#create" #post:データを新規保存するためのHTTPメソッド
   
-  get "/top" => "homes#top"
-
   get "lists" => "lists#index"
   
   get "lists/:id" => 'lists#show', as: "list"
@@ -13,5 +14,7 @@ Rails.application.routes.draw do
   
   get "lists/:id/edit" => "lists#edit", as: "edit_list"
       patch "lists/:id" => "lists#update", as: "update_list"
+  
+  delete "lists/:id" => "lists#destroy", as: "destroy_list"
   
   end
